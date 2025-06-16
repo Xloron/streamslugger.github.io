110d1488c7bf56440df7da723f6982685b714b7b
Phil Berex <pvn@xloron.com>
2025-06-16 20:33:17 +0200

    Updated index.html

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const h1 = document.getElementById('stream-slugger');
   if (h1) {
     h1.addEventListener('click', function() {
-      alert('You clicked on stream slugger!');
-      // Add your custom function here
+      window.location.href = 'index.html';
     });
   }
 });
